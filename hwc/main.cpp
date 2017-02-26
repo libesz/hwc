450e0f7fa9aab646abd8b6cfea97e297dee34339
libesz <huszty.gergo@digitaltrip.hu>
2017-02-26 16:52:42 +0100

    output is now PORTB2 to have INT0 external interrupt

--- hwc/main.cpp
+++ hwc/main.cpp
@@ -21,14 +21,15 @@
 #define TRIGGER_HOLD_OFF_TIME 1200
 //Config END
 
-#define INIT_OUTPUT() DDRB|=1<<3;   // PORTB3 -> output
-#define INIT_TRIGGER() PORTB|=1<<4; // PORTB4 -> input (by default) and activate pull-up
+#define INIT_OUTPUT()  DDRB|=1<<3;                                 // PORTB3 -> output
+#define INIT_TRIGGER() PORTB|=1<<2;MCUCR|=1<<ISC01;GIMSK|=1<<INT0; // PORTB2 -> input (by default), activate pull-up and falling edge interrupt
 
 #define INIT_TIMER0() TCCR0B|=1<<CS02|1<<CS00;TIMSK|=1<<TOIE0; // Set TIMER0 prescaler to 1024; this will cause 3.8 tick/sec (~4HZ)
 #define SECOND_PRESCALER 4
 volatile unsigned int overflow_counter = 0;
 
 SoftTimerSet gSoftTimerSet;
+Trigger *gTrigger = 0;
 
 ISR(TIMER0_OVF_vect) {
 	++overflow_counter;
@@ -40,6 +41,10 @@ ISR(TIMER0_OVF_vect) {
 	}
 }
 
+ISR(INT0_vect) {
+  gTrigger->activate();
+}
+
 int main(void) {
   INIT_OUTPUT();
   INIT_TRIGGER();
@@ -48,10 +53,8 @@ int main(void) {
   gSoftTimerSet.add(&output);
   Trigger trigger(&output, TRIGGER_HOLD_OFF_TIME);
   gSoftTimerSet.add(&trigger);
+  gTrigger = &trigger;
 	sei();
-  while(1) {
-	  if(!(PINB&(1<<4)))
-		  trigger.activate();
-  }
+  while(1);
 }
 
