55291987a8d7d96f8bf364fe258911c1a0fe57ce
libesz <huszty.gergo@digitaltrip.hu>
2017-02-26 17:01:14 +0100

    #define for mask

--- hwc/main.cpp
+++ hwc/main.cpp
@@ -21,7 +21,8 @@
 #define TRIGGER_HOLD_OFF_TIME 1200
 //Config END
 
-#define INIT_OUTPUT()  DDRB|=1<<3;                                 // PORTB3 -> output
+#define OUTPUT_PORT_MASK 1<<3
+#define INIT_OUTPUT()  DDRB|=OUTPUT_PORT_MASK;                                 // PORTB3 -> output
 #define INIT_TRIGGER() PORTB|=1<<2;MCUCR|=1<<ISC01;GIMSK|=1<<INT0; // PORTB2 -> input (by default), activate pull-up and falling edge interrupt
 
 #define INIT_TIMER0() TCCR0B|=1<<CS02|1<<CS00;TIMSK|=1<<TOIE0; // Set TIMER0 prescaler to 1024; this will cause 3.8 tick/sec (~4HZ)
@@ -46,7 +47,7 @@ int main(void) {
   INIT_OUTPUT();
   INIT_TRIGGER();
 	INIT_TIMER0();
-  TimedOutput output((volatile void *)&PORTB, 1<<3, OUTPUT_ON_TIME);
+  TimedOutput output((volatile void *)&PORTB, OUTPUT_PORT_MASK, OUTPUT_ON_TIME);
   gSoftTimerSet.add(&output);
   Trigger trigger(&output, TRIGGER_HOLD_OFF_TIME);
   gSoftTimerSet.add(&trigger);
